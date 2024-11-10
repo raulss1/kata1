e9f88896a57ffc36926923cdfb7054d7e029b747
raulss <raulsanchez1478@gmail.com>
2024-11-10 12:57:28 +0000

    Clase Main y Person ademas de constructor y getter

---
+++ src/software/ulpgc/kata1/Main.java
@@ -0,0 +1,4 @@
+package software.ulpgc.kata1;
+
+public class Main {
+}
---
+++ src/software/ulpgc/kata1/Person.java
@@ -0,0 +1,21 @@
+package software.ulpgc.kata1;
+
+import java.time.LocalDate;
+
+public class Person {
+    private final String name;
+    private final LocalDate birthday;
+
+    public Person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+}
