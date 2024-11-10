6bffaa13aa6d7786bb4cc0d5fea14d98ce41a248
raulss <raulsanchez1478@gmail.com>
2024-11-10 12:59:35 +0000

    Visualizacion de una persona con la clase Main

--- src/software/ulpgc/kata1/Main.java
+++ src/software/ulpgc/kata1/Main.java
@@ -1,4 +1,9 @@
 package software.ulpgc.kata1;
 
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Person("Raúl", LocalDate.of(2003, 8, 22)));
+    }
 }
